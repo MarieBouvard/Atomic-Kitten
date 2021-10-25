4860115cde35fc274cc5fc14dc4c5705867f0fe2
MarieBouvard <mariebouvard@outlook.com>
2021-10-25 10:14:21 +0200

    Add of subtotal and total columns to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_080737) do
+ActiveRecord::Schema.define(version: 2021_10_25_081338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 2021_10_25_080737) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "subtotal"
+    t.decimal "total"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
