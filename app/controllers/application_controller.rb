94d7cf49695121d6912eef134979d3f034fc2106
MarieBouvard <mariebouvard@outlook.com>
2021-10-25 10:47:08 +0200

    Update of application controller and add of cart_items, carts, home, items, orders_items, orders and users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    include ApplicationHelper
+    after_action :create_cart_and_assign_to_current_user, only: [:create]
+
+    protected
+   
+    def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name, :last_name, :email, :password, :password_confirmation])
+       devise_parameter_sanitizer.permit(:account_update, keys: %i[(:first_name, :last_name, :email, :avatar])
+    end
 end
