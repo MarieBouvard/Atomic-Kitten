9dfc29a9d99aaabd95be5d864635453ae840bc0e
MarieBouvard <mariebouvard@outlook.com>
2021-10-25 12:36:16 +0200

    Stripe API keys

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   resources :carts, except: [:index, :new, :edit]
 
 
-
   root to: 'items#index'
+
+  scope 'checkout' do  
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    post 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    post 'success', to: 'checkout#success', as: 'checkout_success'
+  end
 end
