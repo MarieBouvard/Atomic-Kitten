efb6e1cf1537c5663f2a88204d780f9997da4293
MarieBouvard <mariebouvard@outlook.com>
2021-10-25 11:14:47 +0200

    Helpers updates

---
+++ app/helpers/items_helper.rb
@@ -0,0 +1,15 @@
+module ItemsHelper
+    def save_items_in_cart
+        if current_user != nil
+            @items_in_cart = JoinTableItemsCart.where(cart_id:current_user.cart.id)
+        end
+    end
+
+    private
+
+    def only_admin_can_edit_and_add_pictures
+        if !current_user.admin
+            return redirect_to root_path
+        end
+    end
+end
